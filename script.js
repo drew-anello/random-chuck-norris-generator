bb91efeb89d870961c8b242a6edf6b1ac7cb0492
Drew Anello <drew.anello@gmail.com>
2023-02-08 12:11:49 -0500

    data is console.logging

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const url = 'https://api.chucknorris.io/jokes/random'
-
+const request = new Request('https://api.chucknorris.io/jokes/random')
 const btn = document.getElementById('chuckTime')
 const div = document.getElementById('display')
 
 btn.addEventListener('click', function (e) {
   e.preventDefault()
   console.log(e.target)
+  fetch(request)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
 })
